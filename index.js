3cd7828f2894fff11e1fabc438470c377c3b59eb
Stoorny <ahmedreda4000@yahoo.com>
2023-05-04 02:41:21 +0300

    Added Signup and login pages

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
 
 app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.render('layout', { pageTitle: 'Home' });
+});
+app.get('/signup', (req, res) => {
+    res.render('signup', { pageTitle: 'SignUp' });
+});
+app.get('/login', (req, res) => {
+    res.render('login', { pageTitle: 'Login' });
 });
 
 app.listen(3000, () => {
