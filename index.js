eb9803314d428485e5a26918d4f1dcc1eb4737f2
Stoorny <ahmedreda4000@yahoo.com>
2023-05-03 07:44:43 +0300

    Organizing stuff

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
-app.use(express.static('public'));
+app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.render('layout', { pageTitle: 'Home' });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
