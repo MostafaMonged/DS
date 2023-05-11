83df9bee0ae54f01cb99c70d0fe7b62129470777
Ahmed Reda <Ahmedreda4000@yahoo.com>
2023-05-11 22:50:36 +0300

    Testing_Database

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const bcrypt = require('bcrypt');
 const expressLayouts = require('express-ejs-layouts');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = "mongodb+srv://Ahmed200k:Aeuzilua7mDbKK2q@parallelanddistributed.ukthwyn.mongodb.net/?retryWrites=true&w=majority";
+const signupuser = require('./signup.js');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+app.use(express.json());
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.engine('ejs', require('express-ejs-extend')); // add this line
 app.engine('ejs', ejs.renderFile);
@@ -30,4 +60,37 @@ app.get('/checkout', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
+// POSTS
+app.post('/signup', async (req, res) => {
+    try {
+      // Get user data from the request body
+      const { first_name, last_name, email, password, confirm_password, type } = req.body;
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create a new user object with the hashed password
+      const user = {
+        first_name,
+        last_name,
+        username,
+        email,
+        password: hashedPassword,
+        type,
+      };
+  
+      // Connect to MongoDB
+      const client = await MongoClient.connect(url);
+      const db = client.db(Parallel_Project);
+  
+      // Insert the new user into the database
+      const result = await db.collection('Users').insertOne(user);
+  
+      // Close the database connection
+      client.close();
+  
+      res.status(201).json({ message: 'Account created successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
